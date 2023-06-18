38663ebd8bfb38eec714f51891b97cec429f1eef
malarcon-descargas <malarcon.descargas@gmail.com>
2023-06-17 20:41:32 -0400

    Fin Seccion 10

--- models/server.js
+++ models/server.js
@@ -8,6 +8,7 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT || 3000;
+        this.authPath = '/api/auth';
         this.usuariosPath = '/api/usuarios';
 
         // conectar a DB
@@ -35,6 +36,7 @@ class Server {
     }
 
     routes() {
+        this.app.use(this.authPath, require('../routes/auth'));
         this.app.use(this.usuariosPath, require('../routes/usuarios'));
     }
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,7 +1,12 @@
 const {Router} = require('express');
 const {check} = require('express-validator');
 
-const { validarCampos } = require('../middlewares/validar-campos');
+const {
+    validarCampos,
+    validarJWT,
+    tieneRole
+} = require('../middlewares');
+
 const { 
     existeRol, 
     existeEmail, 
@@ -38,6 +43,9 @@ router.put('/:id',[
 ],usuariosPut);
 
 router.delete('/:id',[
+    validarJWT,
+    // esAdminRole,
+    tieneRole('ADMIN_ROLE', 'VENTAR_ROLE','OTRO_ROLE'),
     check('id', 'No es un ID válido').isMongoId(),
     check('id').custom(existeUsuario),
     validarCampos
